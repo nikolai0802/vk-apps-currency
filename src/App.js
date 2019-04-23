1c548bdd09ad54475dc1da35650ad0788eb716da
nikolai0802 <46624533+nikolai0802@users.noreply.github.com>
2019-04-23 19:26:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,86 @@ import React from 'react';
 import {Div, Group, Panel, PanelHeader, View} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
-class App extends React.Component {
+class Example extends React.Component {
+  constructor (props) {
+    super(props);
 
-    render() {
-        return (
-            <View activePanel="mainPanel">
-                <Panel id="mainPanel">
-                    <PanelHeader>Example</PanelHeader>
-                    <Group>
-                        <Div>
-                            Hello world
-                        </Div>
-                    </Group>
-                </Panel>
-            </View>
-        );
-    }
+    this.state = {
+      activeStory: 'more'
+    };
+    this.onStoryChange = this.onStoryChange.bind(this);
+  }
+
+  onStoryChange (e) {
+    this.setState({ activeStory: e.currentTarget.dataset.story })
+  }
+
+  render () {
+
+    return (
+      <Epic activeStory={this.state.activeStory} tabbar={
+        <Tabbar>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'feed'}
+            data-story="feed"
+            text="Новости"
+          ><Icon28Newsfeed /></TabbarItem>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'discover'}
+            data-story="discover"
+            text="Поиск"
+          ><Icon28Search /></TabbarItem>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'messages'}
+            data-story="messages"
+            label="12"
+            text="Сообщения"
+          ><Icon28Messages /></TabbarItem>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'notifications'}
+            data-story="notifications"
+            text="Уведомлен."
+          ><Icon28Notifications /></TabbarItem>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'more'}
+            data-story="more"
+            text="Ещё"
+          ><Icon28More /></TabbarItem>
+        </Tabbar>
+      }>
+        <View id="feed" activePanel="feed">
+          <Panel id="feed">
+            <PanelHeader>Feed</PanelHeader>
+          </Panel>
+        </View>
+        <View id="discover" activePanel="discover">
+          <Panel id="discover">
+            <PanelHeader>Discover</PanelHeader>
+          </Panel>
+        </View>
+        <View id="messages" activePanel="messages">
+          <Panel id="messages">
+            <PanelHeader>Messages</PanelHeader>
+          </Panel>
+        </View>
+        <View id="notifications" activePanel="notifications">
+          <Panel id="notifications">
+            <PanelHeader>Notifications</PanelHeader>
+          </Panel>
+        </View>
+        <View id="more" activePanel="more">
+          <Panel id="more">
+            <PanelHeader>More</PanelHeader>
+          </Panel>
+        </View>
+      </Epic>
+    )
+  }
 }
 
 export default App;
